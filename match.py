d4bc2aec86becfcf69d68ee76a807b10e7c11b5b
Yeou Chiou <yeouchiou@gmail.com>
2018-12-10 20:58:56 -0500

    First commit for match

---
+++ match.py
@@ -0,0 +1,71 @@
+"""
+Computes the closest DM halo counterpart of Gas primary objects
+Usage
+"""
+
+from __future__ import print_function, division
+from sys import argv
+import numpy as np
+import hdf5lib 
+import readsubfHDF5
+import snapHDF5
+
+
+def dx_wrap(dx,box):
+	#wraps to account for period boundary conditions. This mutates the original entry
+	idx = dx > +box/2.0
+	dx[idx] -= box
+	idx = dx < -box/2.0
+	dx[idx] += box 
+	return dx
+def dist2(dx,dy,dz,box):
+	#Calculates distance taking into account periodic boundary conditions
+	return dx_wrap(dx,box)**2 + dx_wrap(dy,box)**2 + dx_wrap(dz,box)**2
+
+# Units
+GRAVITY_cgs = 6.672e-8
+BOLTZMANN = 1.38065e-16
+PROTONMASS = 1.67262178e-24
+GAMMA = 5.0 / 3.0
+GAMMA_MINUS1 = GAMMA - 1.0
+MSUN = 1.989e33
+MPC = 3.085678e24
+KPC = 3.085678e21
+ZSUN = 0.0127
+UnitLength_in_cm = 3.085678e21 # code length unit in cm/h
+UnitMass_in_g = 1.989e43       # code length unit in g/h
+UnitVelocity_in_cm_per_s = 1.0e5
+UnitTime_in_s = UnitLength_in_cm / UnitVelocity_in_cm_per_s
+UnitDensity_in_cgs = UnitMass_in_g/ np.power(UnitLength_in_cm,3)
+UnitPressure_in_cgs = UnitMass_in_g / UnitLength_in_cm / np.power(UnitTime_in_s,2)
+UnitEnergy_in_cgs = UnitMass_in_g * np.power(UnitLength_in_cm,2) / np.power(UnitTime_in_s,2)
+GCONST = GRAVITY_cgs / np.power(UnitLength_in_cm,3) * UnitMass_in_g *  np.power(UnitTime_in_s,2)
+critical_density = 3.0 * .1 * .1 / 8.0 / np.pi / GCONST #.1 is to convert 100/Mpc to 1/kpc, this is in units of h^2
+hubbleparam = .71 #hubble constant
+
+#Should be run with a snap number input
+script, res, vel,  snapnum = argv
+snapnum = int(snapnum) 
+s_vel = vel.replace(".","")
+s_res = res.replace(".","")
+
+#File paths
+filename = "/n/hernquistfs3/mvogelsberger/GlobularClusters/InterfaceWArepo_All_" + res + '_' + vel  + "/output/"
+filename2 = filename +  "DM_FOF" #Used for readsubfHDF5
+filename3 = filename + "snap_" + str(snapnum).zfill(3) #Used for hdf5lib, snapHDF5
+
+#Read header information
+header = snapHDF5.snapshot_header(filename3)
+with hdf5lib.OpenFile(filename3 + ".hdf5") as fs:
+	red = hdf5lib.GetAttr(fs, "Header", "Redshift")
+	atime = hdf5lib.GetAttr(fs, "Header", "Time")
+	boxSize = hdf5lib.GetAttr(fs, "Header", "BoxSize")
+	Omega0 = hdf5lib.GetAttr(fs, "Header", "Omega0")
+	OmegaLambda = hdf5lib.GetAttr(fs, "Header", "OmegaLambda")
+
+#Read halo catalog
+cat = readsubfHDF5.subfind_catalog(filename2, snapnum)
+r200 = cat.Group_R_Crit200
+haloPos = cat.GroupPos
+
+
